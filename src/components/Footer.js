b714c995c177570832e0f65321c0d29a4a6e647b
samiyoc <samiulhaq@youronlineconversation.com>
2021-09-08 17:27:58 +0500

    Iframe Deleted

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -33,7 +33,6 @@ const Footer = () => {
 
         <div className="col-md-3 col-sm-12 mobile-center">
         <h3>Get Direction</h3>
-        {/* <iframe title="Empire Map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.4015369166577!2d67.32999051432422!3d25.054376143629803!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb34b525fa737f9%3A0x43624028ddb19e6!2sEmpire%20Icon!5e0!3m2!1sen!2s!4v1631098983287!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe> */}
         </div>
 
 
